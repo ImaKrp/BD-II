5b3ed660fb71f901beaa77126f5ec9c950878117
marcoduz <marco.duz@estudante.uffs.edu.br>
2025-05-17 15:25:23 -0300

    Adicionar triggers update, delete e commit

--- sql.sql
+++ sql.sql
@@ -29,3 +29,45 @@ CREATE TRIGGER trigger_client_insert
 BEFORE INSERT ON clients
 FOR EACH ROW
 EXECUTE FUNCTION client_insert();
+
+CREATE OR REPLACE FUNCTION client_update()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO clients_log(type, transaction_id, client_id, name, balance)
+    VALUES ('UPDATE', txid_current(), NEW.id, NEW.name, NEW.balance);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_client_update
+BEFORE UPDATE ON clients
+FOR EACH ROW
+EXECUTE FUNCTION client_update();
+
+CREATE OR REPLACE FUNCTION client_delete()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO clients_log(type, transaction_id, client_id, name, balance)
+    VALUES ('DELETE', txid_current(), OLD.id, OLD.name, OLD.balance);
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_client_delete
+BEFORE DELETE ON clients
+FOR EACH ROW
+EXECUTE FUNCTION client_delete();
+
+CREATE OR REPLACE FUNCTION add_commit()
+RETURNS void AS $$
+BEGIN
+    INSERT INTO clients_log (type, transaction_id, client_id, name, balance)
+    VALUES ('COMMIT', txid_current(), NULL, NULL, NULL);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE EVENT TRIGGER trigger_commit
+ON COMMIT
+EXECUTE FUNCTION add_commit();
